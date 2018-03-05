57e31b09bc896927e2a685f763553250053fce0b
akseart <akseart@ya.ru>
2018-03-05 22:46:42 +0300

    change of direction

--- unidirectional list/main.cpp
+++ unidirectional list/main.cpp
@@ -9,7 +9,44 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+    struct unidirectionalList{
+        int data;
+        struct unidirectionalList *next = NULL;
+    };
+    struct unidirectionalList *start;
+    struct unidirectionalList *current = new struct unidirectionalList;
+    struct unidirectionalList *previous;
+    start = current;
+    current->data = 1;
+    for (int i=2;i < 10;i++){
+        previous = current;
+        current = new struct unidirectionalList;
+        previous->next = current;
+        current->data = i;
+    }
+    current = start;
+    while (current != NULL) {
+        std::cout<< " "<<current->data;
+        current = current->next;
+    };
+    
+    previous = NULL;
+    current = start;
+    struct unidirectionalList *next = current->next;
+    while (current != NULL) {
+        current -> next = previous;
+        previous = current;
+        current = next;
+        next = (next == NULL)?NULL:next->next;
+    };
+    start = previous;
+    std::cout<<std::endl;
+    current = start;
+    while (current != NULL) {
+        std::cout<< " "<<current->data;
+        current = current->next;
+    };
+    std::cout <<std::endl<< "Hello, World!\n";
     return 0;
 }
